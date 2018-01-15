936251a385170aba2da61fa49b0816eb41cd3ffa
cBooM <cb@om.com>
2018-01-15 21:51:04 +0000

    gruntfile update

--- gruntfile.js
+++ gruntfile.js
@@ -1,20 +1,11 @@
 module.exports = function(grunt) {
-<<<<<<< HEAD
  
-=======
-
-	const FRONTEND = 'dev';
->>>>>>> 571c9c72a0a9e0c34667dc00b5b8e060400247d0
 
 	require('load-grunt-tasks')(grunt);
 
 	grunt.initConfig({
 		watch: {
-<<<<<<< HEAD
 			files: ['assets/scss/*.scss'],
-=======
-			files: [FRONTEND + '/assets/scss/*.scss'],
->>>>>>> 571c9c72a0a9e0c34667dc00b5b8e060400247d0
 			tasks: ['sass']
 		},
 		sass: {
@@ -23,34 +14,18 @@ module.exports = function(grunt) {
 					loadPath: ['node_modules/foundation-sites/scss/']
 				},
 				files: {
-<<<<<<< HEAD
 					'assets/css/compiled.css': 'assets/scss/main.scss'
-=======
-					'dev/assets/css/compiled.css': 'dev/assets/scss/main.scss'
->>>>>>> 571c9c72a0a9e0c34667dc00b5b8e060400247d0
 				}
 			}
 		},
 		browserSync: {
 			all: {
 				bsFiles: {
-<<<<<<< HEAD
 					src: ['assets/css/*.css', "**/*.php"]
-=======
-					src: ['dev/assets/css/*.css', 'dev/**/*.html']
->>>>>>> 571c9c72a0a9e0c34667dc00b5b8e060400247d0
 				},
 				options: {
-					port: 3020,
 					watchTask: true,
-<<<<<<< HEAD
-					proxy: "localhost/sofi"
-=======
-					startPath: '.',
-					server: {
-						baseDir: FRONTEND
-					}
->>>>>>> 571c9c72a0a9e0c34667dc00b5b8e060400247d0
+					proxy: "localhost/cboom"
 				}
 			}
 		}
